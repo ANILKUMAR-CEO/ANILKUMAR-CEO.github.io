ffd3b7cfe20febf8f6ba930ce53bbefacf3cfe76
ANILKUMAR-CEO <konapuram.anilkumar07@gmail.com>
2026-07-24 08:20:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,17 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 revealOnScroll();
+
+const slides=document.querySelectorAll(".slide");
+
+let currentSlide=0;
+
+setInterval(()=>{
+
+slides[currentSlide].classList.remove("active");
+
+currentSlide=(currentSlide+1)%slides.length;
+
+slides[currentSlide].classList.add("active");
+
+},3000);
