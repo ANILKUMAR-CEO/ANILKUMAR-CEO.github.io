d0b7f4b9c65740c4edd0bc29ee394479e83cba4a
ANILKUMAR-CEO <konapuram.anilkumar07@gmail.com>
2026-07-27 17:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -288,3 +288,13 @@ function filterProducts(type){
     });
 
 }
+
+let cart = [];
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+
+    alert(name + " added to cart!");
+
+    console.log(cart);
+}
