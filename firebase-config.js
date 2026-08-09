81070de6b86b694b71ad4548573665ff598b08f4
ANILKUMAR-CEO <konapuram.anilkumar07@gmail.com>
2026-08-09 14:57:32 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSythwm5e2Q96rDBu4JDdUItAN6nXWW3Q",
+  authDomain: "anil-kumar-dairy-farms.firebaseapp.com",
+  projectId: "anil-kumar-dairy-farms",
+  storageBucket: "anil-kumar-dairy-farms.firebasestorage.app",
+  messagingSenderId: "698324008132",
+  appId: "1:698324008132:web:aaf2b196d4f21c30c91d91",
+  measurementId: "G-42C7RVB715"
+};
